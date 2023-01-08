3c4d29bbc3a0b83e3de9acb64a1d4e81f998f2c9
Sathabhron <thahi5_vi@hotmail.com>
2023-01-08 13:36:58 +0700

    Add files via upload

---
+++ sum_number_thru.cob
@@ -0,0 +1,21 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. ADD-SUM-NUMBERS.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+           01 my-var PIC 99 VALUE 50.
+           01 my-var2 PIC 99 VALUE 50.
+           01 my-var3 PIC 99 VALUE 50.
+           01 resultAdd PIC 999.
+           01 resultMalti PIC 9999.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           PERFORM AddTwoNumbers THRU DisplayResult
+           STOP RUN.
+       AddTwoNumbers.
+           COMPUTE resultAdd = my-var + my-var2 + my-var3.
+       SubtractTwoNumbers.
+           COMPUTE resultMalti = my-var2 * my-var.
+       DisplayResult.
+           DISPLAY "Total: " resultAdd
+           DISPLAY "Multiple: " resultMalti.
+       END PROGRAM ADD-SUM-NUMBERS.
